3126583669c44758e24e9a866b42ce2e575d428a
Rohit Shrivastav <rohitrks805@gmail.com>
2024-07-01 11:33:53 +0530

    assignment added (about | contact page )

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -6,7 +6,9 @@ def home(req):
     return render(req, 'website/index.html')
 
 def about(req):
-    return HttpResponse("Hello World ! You are at chai aur about page...")
+    # return HttpResponse("Hello World ! You are at chai aur about page...")
+    return render(req, 'website/about.html')
 
 def contact(req):
-    return HttpResponse("Hello World ! You are at chai aur contact page...")
+    # return HttpResponse("Hello World ! You are at chai aur contact page...")
+    return render(req, 'website/contact.html')
